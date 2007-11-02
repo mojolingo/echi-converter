f33fa96c1b927c9be4ced4bf99c3fc7a24f4c1d5
Jason Goecke <jason@goecke.net>
2007-11-02 02:14:38 +0000

    Converted main.rb into a Daemon class for Windows service
    git-svn-id: svn://rubyforge.org/var/svn/echi-converter/trunk@70 592edb70-7897-4d5d-b34c-4c60ce0a6027

--- lib/main.rb
+++ lib/main.rb
@@ -1,69 +1,78 @@
 require 'rubygems'
 require 'yaml'
 
-#Determine our working directory
-$workingdir = File.expand_path File.dirname(__FILE__) 
-require $workingdir + '/echi-converter.rb'
-include EchiConverter
+class Daemon
+  #Determine our working directory
+  $workingdir = File.expand_path File.dirname(__FILE__) 
+  require $workingdir + '/echi-converter.rb'
+  include EchiConverter
+    
+  def service_init
+    #Open the configuration file
+    configfile = $workingdir + '/../config/application.yml' 
+    $config = YAML::load(File.open(configfile))
 
-#Open the configuration file
-configfile = $workingdir + '/../config/application.yml' 
-$config = YAML::load(File.open(configfile))
+    #Load ActiveRecord Models
+    if $config["pco_process"] == 'Y'
+      require $workingdir + '/database_presence.rb'
+    else
+      require $workingdir + '/database.rb'
+    end
 
-#Load ActiveRecord Models
-if $config["pco_process"] == 'Y'
-  require $workingdir + '/database_presence.rb'
-else
-  require $workingdir + '/database.rb'
-end
+    #Load the configured schema
+    schemafile = $workingdir + "/../config/" + $config["echi_schema"]
+    @echi_schema = YAML::load(File.open(schemafile))
 
-#Load the configured schema
-schemafile = $workingdir + "/../config/" + $config["echi_schema"]
-@echi_schema = YAML::load(File.open(schemafile))
-
-#Open the logfile with appropriate output level
-initiate_logger
+    #Open the logfile with appropriate output level
+    initiate_logger
   
-#If configured for database insertion, connect to the database
-if $config["export_type"] == 'database' || $config["export_type"] == 'both'
-  connect_database
-end
+    #If configured for database insertion, connect to the database
+    if $config["export_type"] == 'database' || $config["export_type"] == 'both'
+      connect_database
+    end
 
-@log.info "Running..."
+    @log.info "Running..."
+  end
 
-if RUBY_PLATFORM['-mswin']
-  Process::detch
-end
-
-loop do
-  #Process the files
-  ftp_files = fetch_ftp_files
-  #Grab filenames from the to_process directory after an FTP fetch, so if the 
-  #system fails it may pick up where it left off
-  to_process_dir = $workingdir + "/../files/to_process/"
+  def service_loop
+    loop do
+      #Process the files
+      ftp_files = fetch_ftp_files
+      #Grab filenames from the to_process directory after an FTP fetch, so if the 
+      #system fails it may pick up where it left off
+      to_process_dir = $workingdir + "/../files/to_process/"
   
-  #Establish where to copy the processed files to
-  @processeddirectory = set_directory($workingdir)
+      #Establish where to copy the processed files to
+      @processeddirectory = set_directory($workingdir)
   
-  Dir.entries(to_process_dir).each do | file |
-    if file.slice(0,3) == 'chr'
-      if $config["echi_format"] == 'BINARY'
-        record_cnt = convert_binary_file file
-      elsif $config["echi_format"] == 'ASCII'
-        record_cnt = process_ascii file
+      Dir.entries(to_process_dir).each do | file |
+        if file.slice(0,3) == 'chr'
+          if $config["echi_format"] == 'BINARY'
+            record_cnt = convert_binary_file file
+          elsif $config["echi_format"] == 'ASCII'
+            record_cnt = process_ascii file
+          end
+          @log.info "Processed file #{file} with #{record_cnt.to_s} records"
+        end
       end
-      @log.info "Processed file #{file} with #{record_cnt.to_s} records"
+
+      sleep $config["fetch_interval"]
+
+      #Make sure we did not lose our database connection while we slept
+      if ActiveRecord::Base.connected? == 'FALSE'
+        connect_database
+      end
     end
   end
 
-  sleep $config["fetch_interval"]
-
-  #Make sure we did not lose our database connection while we slept
-  if ActiveRecord::Base.connected? == 'FALSE'
-    connect_database
+  def service_cleanup
+    #Close the logfile
+    @log.info "Shutdown..."
+    @log.close
   end
+  
+  d = Daemon.new
+  d.service_init
+  d.service_loop
+  d.service_cleanup
 end
-
-#Close the logfile
-@log.info "Shutdown..."
-@log.close
