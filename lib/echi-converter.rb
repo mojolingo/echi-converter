9ab8f737d215ed969d2a7a283845d5aea3a9ccb4
Jason Goecke <jason@goecke.net>
2009-01-09 17:13:20 +0000

    Fixed a small bug when evaluating a boolean as a string
    git-svn-id: svn://rubyforge.org/var/svn/echi-converter/trunk@191 592edb70-7897-4d5d-b34c-4c60ce0a6027

--- lib/echi-converter.rb
+++ lib/echi-converter.rb
@@ -87,7 +87,7 @@ module EchiConverter
   
   #Method to strip special characters from a string
   def strip_specials(data)
-    if $config["strip_characters"] == 'true'
+    if $config["strip_characters"] == true
       $config["characters_to_strip"].to_a.each do |char|
         data.gsub!(char.chr,"")
       end
